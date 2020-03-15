391d64fc41c94cb190090d6a0898161d6511b194
gabehardgrave <gabriel.hardgrave@gmail.com>
2020-03-15 07:56:22 -0700

    Revert "Revert "Refactor 'Config' to 'Opts'""
    I know I know I can't make up my mind.
    
    This reverts commit 179c1000bbd413959e2a804417c30e32e6b0f0e7.

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use regex::Regex;
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "gsub", about = "Bulk substitutions on a given file")]
-struct Config {
+struct Opts {
     #[structopt(short, long)]
     dry_run: bool,
 
@@ -22,8 +22,8 @@ struct Config {
 }
 
 fn main() -> std::io::Result<()> {
-    let config = Config::from_args();
-    let re = Regex::new(&config.pattern).map_err(|regex_err| {
+    let options = Opts::from_args();
+    let re = Regex::new(&options.pattern).map_err(|regex_err| {
         Error::new(ErrorKind::Other, regex_err.to_string())
     })?;
     let mut file = OpenOptions::new()
@@ -31,12 +31,12 @@ fn main() -> std::io::Result<()> {
         .write(true)
         .append(false)
         .create(false)
-        .open(config.file)?;
+        .open(options.file)?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
 
-    let new_contents = re.replace_all(&contents, config.replacement.as_str());
-    if config.dry_run {
+    let new_contents = re.replace_all(&contents, options.replacement.as_str());
+    if options.dry_run {
         println!("Would have replaced `{}` with `{}`", contents, new_contents);
     } else {
         file.seek(SeekFrom::Start(0))?;
